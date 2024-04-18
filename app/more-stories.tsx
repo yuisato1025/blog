168df14f583ba3b598f6497831c8fb5a3377e1d1
Yui Sato <yuisato.general@gmail.com>
2024-04-19 00:34:04 +0900

    feat: clear default settings

--- app/more-stories.tsx
+++ app/more-stories.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import Avatar from "./avatar";
-import DateComponent from "./date";
-import CoverImage from "./cover-image";
+import Link from 'next/link';
+import Avatar from './avatar';
+import DateComponent from './date';
+import CoverImage from './cover-image';
 
 function PostPreview({
   title,
@@ -41,7 +41,7 @@ export default function MoreStories({ morePosts }: { morePosts: any[] }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+        Coming Soon...
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {morePosts.map((post) => (
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,19 @@
-import Link from "next/link";
-import { draftMode } from "next/headers";
+import Link from 'next/link';
+import { draftMode } from 'next/headers';
 
-import Date from "./date";
-import CoverImage from "./cover-image";
-import Avatar from "./avatar";
-import MoreStories from "./more-stories";
+import Date from './date';
+import CoverImage from './cover-image';
+import Avatar from './avatar';
+import MoreStories from './more-stories';
 
-import { getAllPosts } from "@/lib/api";
-import { CMS_NAME, CMS_URL } from "@/lib/constants";
+import { getAllPosts } from '@/lib/api';
 
 function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Yui Sato's Blog.
       </h1>
-      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          {CMS_NAME}
-        </a>
-        .
-      </h2>
     </section>
   );
 }
